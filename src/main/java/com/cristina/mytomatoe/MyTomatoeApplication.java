6645933c1b546fe7e6b9b4f5434c87b7e9aa0afd
cris <stefandanacristina@gmail.com>
2024-01-31 15:29:34 +0200

    initial commit

---
+++ src/main/java/com/cristina/mytomatoe/MyTomatoeApplication.java
@@ -0,0 +1,13 @@
+package com.cristina.mytomatoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTomatoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTomatoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristina/mytomatoe/MyTomatoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristina.mytomatoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTomatoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
